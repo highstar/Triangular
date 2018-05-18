c3b42e706cd768beb6ca7496dc3ca1e03e7ad649
高星 <gaoxing@gaos-macbook-pro.local>
2018-05-18 10:23:08 +0800

    Initial Commit

---
+++ Triangular/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Triangular
+//
+//  Created by Gao Xing on 2018/5/18.
+//  Copyright © 2018年 Gao Xing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
